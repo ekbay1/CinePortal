8c76faa06bbfc15bf750e4f326d4edc6379dd505
Elias Abay <eliaskabay@gmail.com>
2026-07-06 00:44:15 -0700

    fixing prod issues

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -3,14 +3,25 @@ from sqlalchemy.orm import sessionmaker
 
 from app.core.config import settings
 
-engine = create_engine(settings.database_url)
+from sqlalchemy import create_engine
+from app.core.config import settings
+
+database_url = settings.DATABASE_URL
+
+if database_url.startswith("postgresql://"):
+    database_url = database_url.replace(
+        "postgresql://",
+        "postgresql+psycopg://",
+        1,
+    )
+
+engine = create_engine(database_url)
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine,
 )
-
 
 def get_db():
     db = SessionLocal()
